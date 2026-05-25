24f3b72db0840dad2668723a2c5a6cfe5ca0a931
Rajnish Nath <rnish612@gmail.com>
2026-05-25 21:22:17 +0530

    feat(backend): create multiagent rag pipeline using langgraph

---
+++ backend/graph.py
@@ -0,0 +1,80 @@
+from langgraph.graph import StateGraph, START, END
+from typing import TypedDict, Literal
+from agents import *
+from ingest import retrieve
+
+
+class GraphState(TypedDict):
+    query: str
+    optimized_query: str
+    retrieved_data: str
+    # filtered_data: str
+    final_answer: str
+
+
+builder = StateGraph(GraphState)
+
+
+def optimize_query_node(state: GraphState) -> GraphState:
+    response = query_optimizer.invoke(
+        {"messages": [{"role": "user", "content": f"QUERY: {state['query']}"}]}
+    )
+
+    return {"optimized_query": response["messages"][-1].content}
+
+
+def retriever_node(state: GraphState) -> GraphState:
+    data = retrieve(state["optimized_query"])
+    return {"retrieved_data": data}
+
+
+def filter_node(state: GraphState) -> GraphState:
+    response = retrieve_data_optimizer.invoke(
+        {
+            "messages": [
+                {
+                    "role": "user",
+                    "content": f"QUERY: {state['query']}   RETRIEVED_DATA: {state['retrieved_data']}",
+                }
+            ]
+        }
+    )
+    return {"final_answer": response["messages"][-1].content}
+
+
+def validator_node(state: GraphState) -> Literal["VALID", "INVALID"]:
+    result = query_validator.invoke(
+        {
+            "messages": [
+                {
+                    "role": "user",
+                    "content": f"QUERY: {state['query']}   RETRIEVED_DATA: {state['retrieved_data']}",
+                }
+            ]
+        }
+    )
+    if "INVALID" in result["structured_response"].result:
+        return "INVALID"
+    else:
+        return "VALID"
+
+
+builder.add_node("optimize_query_node", optimize_query_node)
+builder.add_node("retriever_node", retriever_node)
+builder.add_node("filter_node", filter_node)
+builder.add_edge(START, "optimize_query_node")
+builder.add_edge("optimize_query_node", "retriever_node")
+builder.add_conditional_edges(
+    "retriever_node", validator_node, {"INVALID": END, "VALID": "filter_node"}
+)
+builder.add_edge("filter_node", END)
+agent = builder.compile()
+result = agent.invoke(
+    {
+        "query": "what are the best projects of the person",
+        "optimized_query": "",
+        "retrieved_data": "",
+        "final_answer": "",
+    }
+)
+print(result)
--- backend/ingest.py
+++ backend/ingest.py
@@ -2,7 +2,7 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
 
-# from langchain_google_genai.embeddings import GoogleGenerativeAIEmbeddings
+from langchain_google_genai.embeddings import GoogleGenerativeAIEmbeddings
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from dotenv import load_dotenv
 import os
@@ -10,10 +10,10 @@ import os
 load_dotenv()
 
 GOOGLE_GENAI_API = os.getenv("GOOGLE_GENAI_API")
-embedding = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
-# embedding = GoogleGenerativeAIEmbeddings(
-#     api_key=GOOGLE_GENAI_API, model="gemini-embedding-001"
-# )
+# embedding = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+embedding = GoogleGenerativeAIEmbeddings(
+    api_key=GOOGLE_GENAI_API, model="gemini-embedding-001"
+)
 
 
 def build_vector_db(pdfPath: str):
@@ -37,6 +37,7 @@ def retrieve(query: str):
     )
     results = db.similarity_search(query)
     final_result = [result.page_content for result in results]
+    print("final result", final_result)
     return "\n\n".join(final_result)
 
 
